e7df3c97dd2d90a968ae94c175651539c0003d25
Lucàs <vabrelucas@gmail.com>
2022-10-07 19:40:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const search = urlParams.get('s');
+const search = urlParams.get('portfolio');
+
+console.log()
 
 const links = {
-    p: "https://bloom-sidewalk-ccc.notion.site/Hey-I-m-Luc-s-Vabre-f6dd0bde211d4d03b12aff6db0954349"
+    "portfolio": "https://bloom-sidewalk-ccc.notion.site/Hey-I-m-Luc-s-Vabre-f6dd0bde211d4d03b12aff6db0954349"
 }
 
-if (links[search] !== undefined) window.location.replace(links[search]);
+Object.keys(links).forEach(key => {
+    const search = urlParams.get(key);
+    if (search !== null) window.location.replace(links[key]);
+})
