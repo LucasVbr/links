0e3ffa9a8f475be80d26214218bbf4094b831974
Lucàs <vabrelucas@gmail.com>
2022-10-07 19:22:35 +0200

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const search = urlParams.get('to');
+
+const links = {
+    portfolio: "https://bloom-sidewalk-ccc.notion.site/Hey-I-m-Luc-s-Vabre-f6dd0bde211d4d03b12aff6db0954349"
+}
+
+if (links[search] !== undefined) window.location.replace(links[search]);
