e973eb62c9dfd84cd99b775037a0104c76a91136
Lucàs <vabrelucas@gmail.com>
2022-11-12 17:58:13 +0100

    Use Express to redirect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const data = require('../public/data.json');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Home page',
+    links: data,
+  });
+});
+
+/* GET linked pages. */
+data.forEach(link => {
+  router.get(link.stub, (req, res) => res.redirect(link.url));
 });
 
 module.exports = router;
